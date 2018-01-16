8113b8246ab96ebb13f7dc221b3e7a60690f416f
florianmarsch <florianmarsch@192.168.0.143>
2018-01-16 17:52:39 +0100

    organize

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,31 +4,17 @@ import static spark.SparkBase.staticFileLocation;
 
 import java.io.InputStream;
 import java.net.URL;
-import java.text.Normalizer;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang.StringEscapeUtils;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import de.florianmarsch.nfl.Parser;
-import de.florianmarsch.nfl.PlayOffStatus;
 import de.florianmarsch.nfl.Probability;
 import de.florianmarsch.trello.Board;
 import spark.ModelAndView;
-import spark.Spark;
 import spark.template.freemarker.FreeMarkerEngine;
 
 public class Main {
@@ -53,6 +39,9 @@ public class Main {
 			List<Probability> playoffstatus = new Parser().parse(content);
 			
 			List<String> loosers = new ArrayList<String>();
+			
+			
+			
 			
 			for (Probability teamProbabillity : playoffstatus) {
 			
